02d450a92e0caba17047699f82546f7fe5caf9d7
Abdullah Emad <amenesy8@gmail.com>
2024-11-03 22:36:31 +0200

    add LogOut Button

--- lib/app_entry.dart
+++ lib/app_entry.dart
@@ -1,7 +1,9 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:just_chat/core/di/dependency_injection.dart';
 import 'package:just_chat/modules/auth/data/repos/user_data_repo.dart';
 
 import 'core/router/app_router.dart';
@@ -29,16 +31,20 @@ class _JustChatAppState extends State<JustChatApp> with WidgetsBindingObserver {
       onActionReceivedMethod:
           AwesomeNotificationController.onActionReceivedMethod,
     );
-    UserDataRepo.updateUserStatus(true);
+    if (getIt<FirebaseAuth>().currentUser != null) {
+      UserDataRepo.updateUserStatus(true);
+    }
     super.initState();
   }
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      UserDataRepo.updateUserStatus(true);
-    } else if (state == AppLifecycleState.paused) {
-      UserDataRepo.updateUserStatus(false);
+    if (getIt<FirebaseAuth>().currentUser != null) {
+      if (state == AppLifecycleState.resumed) {
+        UserDataRepo.updateUserStatus(true);
+      } else if (state == AppLifecycleState.paused) {
+        UserDataRepo.updateUserStatus(false);
+      }
     }
     super.didChangeAppLifecycleState(state);
   }
