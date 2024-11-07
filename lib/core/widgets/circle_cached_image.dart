c65b71c22ba560f4afc05bb28b8749c0a8b30134
Abdullah Emad <amenesy8@gmail.com>
2024-11-07 22:35:08 +0200

    Solve error:  Image key: CachedNetworkImageProvider("", scale: 1.0)

--- lib/core/widgets/circle_cached_image.dart
+++ lib/core/widgets/circle_cached_image.dart
@@ -2,9 +2,13 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:just_chat/core/constants/image_assets.dart';
+import 'package:just_chat/core/helpers/extensions.dart';
+import 'package:shimmer_animation/shimmer_animation.dart';
+
+import '../theme/colors/colors_manager.dart';
 
 class CircleCachedImage extends StatelessWidget {
-  final String imageUrl;
+  final String? imageUrl;
   const CircleCachedImage({
     super.key,
     required this.imageUrl,
@@ -12,8 +16,16 @@ class CircleCachedImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (imageUrl.isNullOrEmpty()) {
+      // Display a placeholder directly if URL is empty
+      return CircleAvatar(
+        radius: 28.r,
+        backgroundImage: const AssetImage(ImagesAssets.profileHolder),
+      );
+    }
+
     return CachedNetworkImage(
-      imageUrl: imageUrl,
+      imageUrl: imageUrl!,
       height: 56.h,
       width: 56.w,
       imageBuilder: (context, imageProvider) => Container(
@@ -24,8 +36,14 @@ class CircleCachedImage extends StatelessWidget {
           backgroundImage: imageProvider,
         ),
       ),
-      progressIndicatorBuilder: (context, url, downloadProgress) =>
-          CircularProgressIndicator(value: downloadProgress.progress),
+      progressIndicatorBuilder: (context, url, downloadProgress) => Shimmer(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(32.r),
+            color: ColorsManager().colorScheme.primary20,
+          ),
+        ),
+      ),
       errorWidget: (context, url, error) =>
           Image.asset(ImagesAssets.profileHolder),
     );
